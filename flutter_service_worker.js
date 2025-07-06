595a868f6b6abb522cabdbf73ce6457fecb0d03f
Talkaroo <toastywoastyy123@gmail.com>
2025-07-06 09:36:39 -0400

    update site with latest build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -74,7 +74,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9dac8c41e4b3148d6c98fc59ecdb928d
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c742f1dd802ac74df407fbc425a1da18",
+"flutter_bootstrap.js": "f69a91c6ddbfb3ed8d43b96a65abf36c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
